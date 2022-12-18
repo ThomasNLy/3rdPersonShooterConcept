c90a3f11c83d932831d60a55c9773532c10404f0
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2022-12-18 10:52:13 -0500

    created a script for having a grenade explode and deal damage to enemies

---
+++ Assets/Scripts/Weapon Scripts/Grenade.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+
+    [Header("Fx Refference")]
+    public GameObject explosionFX;
+
+    [Header("Grenade Variables")]
+    [SerializeField]
+    private bool exploded;
+    private bool startFuse;
+    [SerializeField]
+    float durationTimer;
+    [SerializeField]
+    float explosionTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionFX.GetComponent<ParticleSystem>().Stop();
+        exploded = false;
+        startFuse = false;
+        durationTimer = explosionFX.GetComponent<ParticleSystem>().main.duration;
+        explosionTimer = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (!startFuse) 
+        {
+            startFuse = true;
+            Invoke("Explode", explosionTimer);
+        }
+
+        //if (explosionFX.GetComponent<ParticleSystem>().isPlaying) /*&& !explosionFX.GetComponent<ParticleSystem>().isPlaying)*/
+        //{
+        //    Destroy(this.gameObject, durationTimer);
+        //}
+
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Enemy" && exploded)
+        {
+            other.GetComponent<Health>().TakeDamage(10);
+        }
+    }
+
+
+    void Explode()
+    {
+        exploded = true;
+        explosionFX.GetComponent<ParticleSystem>().Play();
+        Debug.Log("Explosion");
+        Destroy(this.gameObject, durationTimer);
+    }
+}
