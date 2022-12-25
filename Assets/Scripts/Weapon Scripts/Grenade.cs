f4f92975c630b9c3e90b2e9f5a77b03815b7aba9
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2022-12-24 22:40:11 -0500

    updated grenade to work with overlapsphere instead of collider
    The grenade will now only deal damage to enemies that aren't behind cover using overlap sphere to check for all collider's in it's radius. Also will check by throwing a ray cast in the enemies direction to see if it isn't behind something in order to deal damage.
    
    Added a OnDrawGizmos in order top see the blast radius of the grenade.

--- Assets/Scripts/Weapon Scripts/Grenade.cs
+++ Assets/Scripts/Weapon Scripts/Grenade.cs
@@ -7,11 +7,9 @@ public class Grenade : MonoBehaviour
 
     [Header("Fx Refference")]
     public GameObject explosionFX;
-
-    [Header("Grenade Variables")]
     [SerializeField]
-    private bool exploded;
     private bool startFuse;
+    [Header("Grenade Variables")]
     [SerializeField]
     float durationTimer;
     [SerializeField]
@@ -20,41 +18,59 @@ public class Grenade : MonoBehaviour
     void Start()
     {
         explosionFX.GetComponent<ParticleSystem>().Stop();
-        exploded = false;
-        startFuse = false;
         durationTimer = explosionFX.GetComponent<ParticleSystem>().main.duration;
         explosionTimer = 5f;
+        startFuse = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        if (!startFuse) 
+
+        if (!startFuse)
         {
             startFuse = true;
+            // will trigger explosion after set timer 
             Invoke("Explode", explosionTimer);
         }
-
-
-
+        
 
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Enemy" && exploded)
-        {
-            other.GetComponent<Health>().TakeDamage(10);
-        }
-    }
 
 
     void Explode()
     {
-        exploded = true;
+        //this.transform.rotation = Quaternion.Euler(0, 0, 0);
         explosionFX.GetComponent<ParticleSystem>().Play();
         Debug.Log("Explosion");
+        DealDamage();
         Destroy(this.gameObject, durationTimer);
+    }
+
+
+    void DealDamage()
+    {
+        Collider[] itemsInArea = Physics.OverlapSphere(this.transform.position, 7);
+        foreach (Collider item in itemsInArea)
+        {
+
+            Vector3 direction = (item.transform.position - this.transform.position).normalized;
+            if (Physics.Raycast(this.transform.position, direction, out RaycastHit itemHit, 7))
+            {
+                if (itemHit.transform.tag == "Enemy")
+                {
+                    itemHit.transform.GetComponent<Health>().TakeDamage(10);
+                }
+            }
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.5f);
+        Gizmos.DrawSphere(this.transform.position, 7);
+
     }
 }
