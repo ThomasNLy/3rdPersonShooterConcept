af9ba6b9ca2794c4c3128c66a9f58c5e1810d524
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2022-12-24 22:45:48 -0500

    fixed name typo

--- Assets/Scripts/Weapon Scripts/ShootMissile.cs
+++ Assets/Scripts/Weapon Scripts/ShootMissile.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ShootMissle : MonoBehaviour
+public class ShootMissile : MonoBehaviour
 {
     Mouse mouse;
     [Header("Prefab Reference")]
