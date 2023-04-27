2ac6a0521cdcbca69bc9c12fbadeffefd28bdd2a
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2023-04-27 16:20:04 -0400

    add a small jump after grappling onto an item
    when grappling towards a wall or barrier after hitting it the player will jump up a bit allowing them to climb over ledges

--- Assets/Scripts/GrappleHook.cs
+++ Assets/Scripts/GrappleHook.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class GrappleHook : ShootingRayCast2
 {
     Keyboard keyboard;
+    [Header("Character controller component")]
     [SerializeField]
     CharacterController controller;
     [Header("Grapple Hook Variables")]
@@ -13,6 +14,9 @@ public class GrappleHook : ShootingRayCast2
     private bool _usingGrapple;
     [SerializeField]
     private int speed = 12;
+
+    [SerializeField]
+    private bool hitObject;
 
     [SerializeField]
     private LineRenderer lineRender;
@@ -55,6 +59,24 @@ public class GrappleHook : ShootingRayCast2
 
         MoveTowardsTarget();
 
+
+        // checks if the player lands back on the ground after jumping up a bit after hitting an object i.e. wall and stops them from continually launching up.
+        if (controller.isGrounded)
+
+        {
+            Debug.Log("fell back down to ground");
+            hitObject = false;
+        }
+
+
+        //have the player jump up a bit after hitting an object ie climb over a small ledge. 
+        // can't be using the grapple hook for this to work: allows the player to chain grappling 
+        if (hitObject && !usingGrapple)
+        {
+            controller.Move(new Vector3(0, 10, 0) * Time.deltaTime);
+        }
+       
+
     }
 
     protected override void DoSomethingWhenHit()
@@ -94,6 +116,15 @@ public class GrappleHook : ShootingRayCast2
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
+        //checks to see is using the grapple hook and the player isn't on the ground to have them jump up after colliding with an object
+        // prevents player form jumping by accident if they are grappling towards the floor 
+        if (usingGrapple && controller.isGrounded == false)
+        {
+           Debug.Log("hit the wall");
+           hitObject = true;
+        }
+
         usingGrapple = false;
+        
     }
 }
